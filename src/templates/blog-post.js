4bb931a4428ca305153be924a0e22e48d55307e9
YutaGoto <you.goto.510@gmail.com>
2021-06-26 22:55:34 +0900

    :tada: upgrade packages

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -42,7 +42,7 @@ const BlogPostTemplate = ({ data, location }) => {
             </article>
             <div className="columns">
               <div className="column is-half">
-                <div class="mt-5 mb-5">
+                <div className="mt-5 mb-5">
                   <TwitterShareButton url={`${siteMetadata.siteUrl}/blog${post.fields.slug}`} title={`${post.frontmatter.title} - .ごっちの日記`} className="m-2">
                     <TwitterIcon size="32" round />
                   </TwitterShareButton>

b378a98b765ebc34e50f137debcbb152e5c4c2dd
YutaGoto <you.goto.510@gmail.com>
2021-12-20 23:53:04 +0900

    upgrade gatsby js

--- src/components/buttonLink.js
+++ src/components/buttonLink.js
@@ -4,7 +4,7 @@ import "./fontawesome"
 
 export const ButtonLink = ({ text, linkTo, iconName, isLeft = true }) => (
   <Link
-    className="box-border p-2 lg:mb-0 mb-1 border text-button dark:text-button-dark border-button dark:border-blue-700 hover:bg-button hover:text-main dark:hover:bg-button-dark dark:hover:text-main-dark"
+    className="box-border p-2 lg:mb-0 mb-1 border text-blue-400 dark:text-gray-100 border-blue-400 dark:border-blue-700 hover:bg-blue-400 hover:text-gray-900 dark:hover:bg-blue-700 dark:hover:text-gray-100"
     to={linkTo}
   >
     {isLeft && <i className={`fas fa-${iconName}`} aria-hidden="true" />}

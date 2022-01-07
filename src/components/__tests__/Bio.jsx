d75a7bc1bb101e7c3b4545c2d710ac87671618c7
YutaGoto <you.goto.510@gmail.com>
2022-01-07 21:57:00 +0900

    refactor: some components move to jsx

--- src/components/__tests__/Bio.jsx
+++ src/components/__tests__/Bio.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import renderer from "react-test-renderer"
 import { render } from "@testing-library/react"
-import { PureBio as Bio } from "../bio"
+import { PureBio as Bio } from "../Bio"
 
 describe("bio", () => {
   const data = {
---
+++ src/components/index.jsx
@@ -0,0 +1,6 @@
+import { ArticleCard } from "./ArticleCard"
+import { Bio } from "./Bio"
+import { ButtonLink } from "./ButtonLink"
+import { Layout } from "./Layout"
+
+export { ArticleCard, Bio, ButtonLink, Layout }

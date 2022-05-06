86288b2c34312cfed0b4049bcbb6cbae5beb3bf8
YutaGoto <you.goto.510@gmail.com>
2022-05-06 14:18:03 +0900

    :lipstick: fix tag text color

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -9,7 +9,9 @@ interface TagProps {
 
 export const Tag = ({ tag, className = "", withLink = true }: TagProps) => {
   return (
-    <span className={`${className} py-1 px-2 rounded text-sm bg-emerald-800`}>
+    <span
+      className={`${className} text-gray-100 py-1 px-2 rounded text-sm bg-emerald-800`}
+    >
       {withLink ? <Link to={`/tags/${tag}`}>{tag}</Link> : tag}
     </span>
   )

57ef459c38284a3da5027d3465260fc083cfb5d3
YutaGoto <you.goto.510@gmail.com>
2022-12-30 21:10:47 +0900

    :heavy_minus_sign: remove gatsby files

--- gatsby-browser.ts
+++
@@ -1,8 +0,0 @@
-// custom typefaces
-import "typeface-montserrat"
-import "typeface-merriweather"
-// custom CSS styles
-import "./src/style.scss"
-
-// Highlighting for code blocks
-import "prism-themes/themes/prism-atom-dark.css"

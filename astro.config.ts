de557e91ab7466226f909e73485a4c761b27b4b1
YutaGoto <you.goto.510@gmail.com>
2023-10-01 15:42:12 +0900

    :wrench: set partytown

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from "astro/config"
+import { defineConfig } from "astro/config";
+import partytown from "@astrojs/partytown";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [],
+  integrations: [partytown()],
   site: "https://yutagoto.github.io",
-  base: "/blog",
-})
+  base: "/blog"
+});

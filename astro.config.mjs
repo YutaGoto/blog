7ba7ee28f7df46b059b26a8198c565bf13084950
YutaGoto <12590762+YutaGoto@users.noreply.github.com>
2024-03-27 22:42:26 +0900

    :rotating_light: fix config lint

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
-import partytown from "@astrojs/partytown";
-import tailwind from "@astrojs/tailwind";
-import { defineConfig } from "astro/config";
+import partytown from "@astrojs/partytown"
+import tailwind from "@astrojs/tailwind"
+import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     partytown({
       config: {
-        forward: ["dataLayer.push"]
-      }
+        forward: ["dataLayer.push"],
+      },
     }),
     tailwind(),
   ],
   site: "https://blog.yougoto.dev",
-});
+})

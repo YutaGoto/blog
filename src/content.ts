6f3449f213ba093df37f6ec30ad9148c309c44fb
YutaGoto <12590762+YutaGoto@users.noreply.github.com>
2025-01-13 11:37:35 +0900

    :mag: set RSS setting

--- src/content.ts
+++ src/content.ts
@@ -1,3 +1,8 @@
-import { getCollection } from "astro:content"
+import { defineCollection, getCollection } from "astro:content"
+import { rssSchema } from "@astrojs/rss"
+
+export const blogCollection = defineCollection({
+  schema: rssSchema,
+})
 
 export const allPosts = await getCollection("posts")

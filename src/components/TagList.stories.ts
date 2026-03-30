e69cd36bfac8b6ac5020f42c7c5409dd47337d20
YutaGoto <12590762+YutaGoto@users.noreply.github.com>
2026-03-30 23:49:35 +0900

    :label: add stories type

--- src/components/TagList.stories.ts
+++ src/components/TagList.stories.ts
@@ -1,5 +1,8 @@
+import type { ComponentProps } from "astro/types"
 import TagList from "./TagList.astro"
 import "remixicon/fonts/remixicon.css"
+
+type TagListProps = ComponentProps<typeof TagList>
 
 export default {
   component: TagList,
@@ -8,12 +11,12 @@ export default {
 export const Default = {
   args: {
     tags: ["デフォルト", "デフォルト2"],
-  },
+  } satisfies TagListProps,
 }
 
 export const Active = {
   args: {
     tags: ["デフォルト", "デフォルト2"],
     activeTag: "デフォルト",
-  },
+  } satisfies TagListProps,
 }

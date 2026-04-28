991f5e13c459eebe328d06a6187ddc5d33c9e23b
YutaGoto <12590762+YutaGoto@users.noreply.github.com>
2026-04-29 01:05:25 +0900

    :zap: fix unocss config and style

--- uno.config.ts
+++ uno.config.ts
@@ -1,8 +1,8 @@
 import {
   defineConfig,
   presetMini,
-  presetUno,
-  presetWind,
+  presetTypography,
+  presetWind4,
   transformerDirectives,
 } from "unocss"
 
@@ -13,5 +13,52 @@ export default defineConfig({
     },
   },
   transformers: [transformerDirectives()],
-  presets: [presetUno(), presetWind(), presetMini()],
+  presets: [
+    presetMini(),
+    presetWind4(),
+    presetTypography({
+      cssExtend: {
+        h1: {
+          color: "inherit",
+          "font-size": "2.25rem",
+          "line-height": "2.5rem",
+          "font-weight": "700",
+          "padding-top": "0.5rem",
+          "padding-bottom": "0.5rem",
+        },
+        h2: {
+          color: "inherit",
+          "font-size": "1.875rem",
+          "line-height": "2.25rem",
+          "font-weight": "700",
+          "padding-top": "0.5rem",
+          "padding-bottom": "0.5rem",
+        },
+        h3: {
+          color: "inherit",
+          "font-size": "1.5rem",
+          "line-height": "2rem",
+          "font-weight": "500",
+          "padding-top": "0.5rem",
+          "padding-bottom": "0.5rem",
+        },
+        p: {
+          color: "inherit",
+          "font-size": "1.125rem",
+          "line-height": "1.75rem",
+          "font-weight": "400",
+        },
+        li: {
+          color: "inherit",
+          "font-size": "1.125rem",
+          "line-height": "1.75rem",
+          "font-weight": "400",
+        },
+        a: {
+          color: "inherit",
+          "text-decoration": "underline",
+        },
+      },
+    }),
+  ],
 })
